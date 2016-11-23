8869581c59e73f566961853a2459732ece0bb2ec
Ivan Menshikh <menshikh.iv@gmail.com>
2016-11-23 19:36:03 +0500

    optimization

--- hw-2/betweenness_centrality.cpp
+++ hw-2/betweenness_centrality.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <vector>
 #include <queue>
 #include <stack>
@@ -7,14 +7,15 @@
 #include <ctime>
 #include <limits.h>
 #include <stdlib.h>
+#include <math.h>
 #include <sys/time.h>
 #include <omp.h>
 
 using namespace std;
 
 
-map<int, vector<int>> erdos_renyi_graph(int n, float p){
-	map<int, vector<int>> graph;
+unordered_map<int, vector<int>> erdos_renyi_graph(int n, float p){
+	unordered_map<int, vector<int>> graph;
 
 	for (int i = 0; i < n; i++){
 		graph.insert(pair<int, vector<int>>(i, vector<int>()));
@@ -32,7 +33,7 @@ map<int, vector<int>> erdos_renyi_graph(int n, float p){
 }
 
 
-void print_graph(map<int, vector<int>> graph){
+void print_graph(unordered_map<int, vector<int>> graph){
 	for(auto elem : graph){
    		cout << elem.first << " ";
    		for (auto i : elem.second){
@@ -43,17 +44,17 @@ void print_graph(map<int, vector<int>> graph){
 }
 
 struct sssp_retval{
-	map<int, vector<int>> pred;
+	unordered_map<int, vector<int>> pred;
 	int* sigma;
 	stack<int> _s;
 };
 
-sssp_retval singe_source_shortes_paths(map<int, vector<int>> graph, int s){
+sssp_retval singe_source_shortes_paths(unordered_map<int, vector<int>>& graph, int s){
 	int n = graph.size();
 
 	int* dist = new int[n];
 	int* sigma = new int[n];
-	map<int, vector<int>> pred;
+	unordered_map<int, vector<int>> pred;
 
 	fill_n(dist, n, INT_MAX);
 	fill_n(sigma, n, 0);
@@ -95,8 +96,8 @@ sssp_retval singe_source_shortes_paths(map<int, vector<int>> graph, int s){
 	return res;
 }
 
-void accumulation(int n, int s, sssp_retval d, float* btw){
-	float* delta = new float[n];
+void accumulation(int n, int s, sssp_retval& d, double* btw){
+	double* delta = new double[n];
 	fill_n(delta, n, 0.);
 
 	while (!d._s.empty()){
@@ -104,51 +105,70 @@ void accumulation(int n, int s, sssp_retval d, float* btw){
 		d._s.pop();
 
 		for (int v: d.pred[w]){
-			delta[v] += ((float)d.sigma[v] * (1. + delta[w])) / (float)d.sigma[w];
+			delta[v] += ((double)d.sigma[v] * (1. + (double)delta[w])) / (double)d.sigma[w];
 		}
 
 		if (w != s){
+			#pragma omp atomic
             btw[w] += delta[w];
+        
 		}
 	}
 	delete[] delta;
+}
+
+void betweenness_centrality_serial(unordered_map<int, vector<int>>& graph, double* btw){
+	int n = graph.size();
+
+	for (int s = 0; s < n; s++){
+		sssp_retval rv = singe_source_shortes_paths(graph, s);
+		accumulation(n, s, rv, btw);
+	}
+}
+
+void betweenness_centrality_parallel(unordered_map<int, vector<int>>& graph, double* btw){
+	int n = graph.size();
+
+	#pragma omp parallel for
+	for (int s = 0; s < n; s++){
+		sssp_retval rv = singe_source_shortes_paths(graph, s);
+		accumulation(n, s, rv, btw);
+	}
+}
+
+
+bool assert_equal(double* arr1, double* arr2, int n){
+	for (int i = 0; i < n; i++){
+		if (fabs(arr1[i] - arr2[i]) > 1e-7){
+			cout << i << " | " << arr1[i] << ", " << arr2[i] << endl; 
+			return false;
+		}
+	}
+	return true;
+}
+
+bool test_correct_parallel(unordered_map<int, vector<int>>& graph){
+	int n = graph.size();
+
+	double* btw_serial = new double[n];
+	double* btw_parallel = new double[n];
+	fill_n(btw_serial, n, 0.);
+	fill_n(btw_parallel, n, 0.);
+
+	betweenness_centrality_serial(graph, btw_serial);
+	betweenness_centrality_parallel(graph, btw_parallel);
+
+	return assert_equal(btw_serial, btw_parallel, n);
 }
 
 
 int main(){
 	srand (static_cast <unsigned> (time(0)));
 
-	float sssp_sec = 0.;
-	float acc_sec = 0.;
-
 	int n = 1000;
 	float p = (n * 10.) / (n * (n - 1) * 0.5);
 
 	auto graph = erdos_renyi_graph(n, p);
-	//print_graph(graph);
 
-	float* btw = new float[n];
-	fill_n(btw, n, 0.);
-
-	for (int s = 0; s < n; s++){
-		struct timeval start_s, end_s;
-
-        gettimeofday(&start_s, NULL);
-		sssp_retval rv = singe_source_shortes_paths(graph, s);
-		gettimeofday(&end_s, NULL);
-		sssp_sec += ((end_s.tv_sec  - start_s.tv_sec) * 1000000u + end_s.tv_usec - start_s.tv_usec);
-
-		gettimeofday(&start_s, NULL);
-		accumulation(n, s, rv, btw);
-		gettimeofday(&end_s, NULL);
-		acc_sec += ((end_s.tv_sec  - start_s.tv_sec) * 1000000u + end_s.tv_usec - start_s.tv_usec);
-
-	}
-	/*
-	for (auto el: btw){
-		cout << el.first << " - " << el.second / 2. << endl;
-	}*/
-
-	cout << "sssp_sec: "<< sssp_sec / 1.e6 << " acc_sec: "<< acc_sec / 1.e6 << endl;
-	return 0;
+	cout << "Is correct: " << test_correct_parallel(graph) << endl;
 }
